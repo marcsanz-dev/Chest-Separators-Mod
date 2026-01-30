959c82bfbb48bd8f6f560fbc3a93c2f01ed84f7a
marcsanz-dev <marcsanzp@gmail.com>
2026-01-30 09:46:10 +0100

    Initial commit: Project skeleton generated by Fabric

---
+++ src/client/java/io/github/marcsanzdev/chestseparators/client/ChestseparatorsClient.java
@@ -0,0 +1,10 @@
+package io.github.marcsanzdev.chestseparators.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ChestseparatorsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/io/github/marcsanzdev/chestseparators/Chestseparators.java
@@ -0,0 +1,17 @@
+package io.github.marcsanzdev.chestseparators;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Chestseparators implements ModInitializer {
+
+    public static final String MOD_ID = "chestseparators";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("¡Chest Separators se ha cargado correctamente!");
+    }
+}
